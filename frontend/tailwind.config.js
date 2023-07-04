5899cc7a18cf61adf6e44f396664ea2a0ca1c89f
chmanie <code@chmanie.com>
2023-07-04 17:24:19 +0200

    Update tailwind theme, first html template

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./www/*.html', './src/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      blue: {
+        1000: '#021C2B',
+        900: '#012138',
+        800: '#00284B',
+        400: '#289BDC',
+      },
+      grey: {
+        500: '#A4B6C7',
+      },
+      pink: '#F5416E',
+      purple: {
+        1000: '#101828',
+        500: '#5865F2',
+      },
+      transparent: 'transparent',
+      white: '#ffffff',
+    },
+    fontFamily: {
+      sans: ['Avenir Next', 'sans-serif'],
+      mono: ['IBM Plex Mono', 'monospace'],
+    },
   },
   plugins: [],
 }
