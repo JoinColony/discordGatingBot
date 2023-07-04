d0caae7aa5bd72d4665d71a032af837b2cf11572
chmanie <code@chmanie.com>
2023-07-05 00:57:26 +0200

    Add initial frontend design

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,36 +1,73 @@
 import { BrowserProvider } from 'ethers';
 
-// If MetaMask is installed there will be an `ethereum` object on the `window`
-const provider = new BrowserProvider((window as any).ethereum);
+const connectButton = document.querySelector('#button-connect') as HTMLButtonElement;
+const disconnectButton = document.querySelector('#button-disconnect') as HTMLButtonElement;
+const errorText = document.querySelector('#text-error') as HTMLParagraphElement;
+const successText = document.querySelector('#text-success') as HTMLParagraphElement;
 
-// Get the Colony's XDAI funding in the ROOT pot (id 1)
-const start = async () => {
-  // This will try to connect the page to MetaMask
-  await provider.send('eth_requestAccounts', []);
+const pathSplit = window.location.pathname.split('/');
+const username = pathSplit[2]
+const sessionId = pathSplit[3]
 
-  const signer = await provider.getSigner();
-  const address = await signer.getAddress();
-
-  const pathSplit = window.location.pathname.split('/');
-  const username = pathSplit[2]
-  const sessionId = pathSplit[3]
-  const signature = await signer.signMessage(`Please sign this message to connect your Discord username ${username} with your wallet address. Session ID: ${sessionId}`);
-
-  const response = await fetch(window.location.href, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      signature,
-      address,
-    })
-  });
-
-  if (response.ok) {
-    alert('Successfully connected!');
+if (connectButton) {
+  if (username) {
+    connectButton.innerText = `${connectButton.innerText} as ${username}`;
   }
-};
+  connectButton.style.visibility = 'visible';
 
-// start();
+  connectButton.addEventListener('click', async () => {
+    connectButton.disabled = true;
+
+    // If MetaMask is installed there will be an `ethereum` object on the `window`
+    const provider = new BrowserProvider((window as any).ethereum);
+
+    // This will try to connect the page to MetaMask
+    await provider.send('eth_requestAccounts', []);
+
+    const signer = await provider.getSigner();
+    const address = await signer.getAddress();
+
+    const signature = await signer.signMessage(`Please sign this message to connect your Discord username ${username} with your wallet address. Session ID: ${sessionId}`);
+
+    const response = await fetch(window.location.href, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        signature,
+        address,
+      })
+    });
+
+    if (response.ok) {
+      connectButton.style.visibility = 'hidden';
+      successText.style.visibility = 'visible';
+      errorText.style.visibility = 'hidden';
+    } else {
+      errorText.style.visibility = 'visible';
+      connectButton.disabled = false;
+    }
+  });
+} else if (disconnectButton) {
+  if (username) {
+    disconnectButton.innerText = `${disconnectButton.innerText} as ${username}`;
+  }
+  disconnectButton.style.visibility = 'visible';
+
+  disconnectButton.addEventListener('click', async () => {
+    disconnectButton.disabled = true;
+    const response = await fetch(window.location.href, {
+      method: 'POST'
+    });
+    if (response.ok) {
+      disconnectButton.style.visibility = 'hidden';
+      successText.style.visibility = 'visible';
+      errorText.style.visibility = 'hidden';
+    } else {
+      errorText.style.visibility = 'visible';
+      disconnectButton.disabled = false;
+    }
+  });
+}
